9fd3741195e2654df8e87cdc54074fb696e20451
Pratyay Sarkar <pratyay2sarkar@gmail.com>
2022-04-01 20:44:03 +0530

    Added logout button for placeholder Home Screen

--- lib/components/home_screen/root_component.dart
+++ lib/components/home_screen/root_component.dart
@@ -1,12 +1,16 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:frontend/providers/user_provider.dart';
 import 'package:provider/provider.dart';
+
+import '../../utils/auth_utils.dart';
 
 class RootComponent extends StatelessWidget {
   const RootComponent({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    final FirebaseAuth auth = FirebaseAuth.instance;
     return Scaffold(
       body: SafeArea(
         child: Column(
@@ -15,6 +19,13 @@ class RootComponent extends StatelessWidget {
               "${Provider.of<UserProvider>(context).loggedInUser!.currentBalance}",
               key: const Key("user_current_value"),
             ),
+            ElevatedButton(
+                onPressed: () async {
+                        await Auth(authInstance: auth).logoutUser();
+                        Navigator.pushReplacementNamed(context, '/welcome');
+                      },
+                child: const Text('Logout')
+            )
           ],
         ),
       ),
