e3d46b891eba32e35073aab81fd4294a1e7e489b
dhruvroytalukdar <dhruvroy8@gmail.com>
2022-04-06 23:34:04 +0530

    logout button added did small ui changes

--- lib/components/home_screen/drawer_component/drawer_component.dart
+++ lib/components/home_screen/drawer_component/drawer_component.dart
@@ -1,13 +1,17 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:frontend/components/home_screen/drawer_component/drawer_footer.dart';
 import 'package:frontend/components/home_screen/drawer_component/drawer_header.dart';
 import 'package:frontend/components/home_screen/drawer_component/drawer_list_item.dart';
+import 'package:frontend/utils/auth_utils.dart';
 
 class DrawerComponent extends StatelessWidget {
   const DrawerComponent({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    FirebaseAuth auth = FirebaseAuth.instance;
+
     return Drawer(
       backgroundColor: Colors.white,
       child: Column(
@@ -40,6 +44,15 @@ class DrawerComponent extends StatelessWidget {
                 title: "Withdraw",
                 onClick: () {
                   Navigator.pop(context);
+                },
+              ),
+              DrawerListComponent(
+                title: "Logout",
+                onClick: () async {
+                  await Auth(authInstance: auth).logoutUser();
+                  // Provider.of<UserProvider>(context, listen: false).resetUser();
+                  Navigator.pop(context);
+                  Navigator.pushReplacementNamed(context, '/welcome');
                 },
               ),
             ],
