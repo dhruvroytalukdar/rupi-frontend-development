9086109b67ddd51f30db7d33593bcef53a9989a1
Pratyay Sarkar <pratyay2sarkar@gmail.com>
2022-04-07 01:24:09 +0530

    Bank Details Component UI Finished, Widget Tests for State 1, TODO: Remaining States' Widget Tests

--- test/bank_details_test.dart
+++ test/bank_details_test.dart
@@ -1,8 +1,29 @@
 import 'package:flutter_test/flutter_test.dart';
 import 'package:flutter/material.dart';
 import 'package:frontend/components/home_screen/bank_details_component.dart';
+import 'package:frontend/constants/index.dart';
+import 'package:frontend/providers/user_provider.dart';
+import 'package:frontend/providers/user_status_provider.dart';
+import 'package:provider/provider.dart';
 
 void main(){
+
+  // Utility function to load the screen in a material app
+  _pumpWidget(WidgetTester tester) async {
+    await tester.pumpWidget(
+      MaterialApp(
+        title: 'Flutter Demo',
+        home: MultiProvider(
+          providers: [
+            ChangeNotifierProvider(create: (_) => UserProvider(testUser)),
+            ChangeNotifierProvider(
+                create: (_) => UserStatusProvider(true, false, false, false)),
+          ],
+          child: const BankDetailsComponent(),
+        ),
+      ),
+    );
+  }
 
   testWidgets('Continue button is showing correctly or not.', (WidgetTester tester) async {
     // Get the widget
@@ -51,7 +72,7 @@ void main(){
     expect(ifscCodeField,findsOneWidget);
   });
 
-  testWidgets('IFSC Text field is showing correctly or not.', (WidgetTester tester) async {
+  testWidgets('UPI Text field is showing correctly or not.', (WidgetTester tester) async {
     // Get the widget
     final upiCodeField = find.byKey(const Key('upiCode'));
 
@@ -62,4 +83,34 @@ void main(){
     // Test Results
     expect(upiCodeField,findsOneWidget);
   });
-}
+
+    testWidgets(
+        'Deposit button is showing correctly or not, after clicking continue twice', (
+        WidgetTester tester) async {
+      // Get the widget
+      final depositButton = find.byKey(
+          const Key('deposit'));
+      final bankDetailsContinueState1Button = find.byKey(const Key('bankDetailsContinueState1'));
+      final name = find.byKey(const Key('name'));
+      final accountNumber = find.byKey(const Key('accNo'));
+      final ifscCodeField = find.byKey(const Key('ifscCode'));
+      final upiCodeField = find.byKey(const Key('upiCode'));
+
+      // Build the app
+      await _pumpWidget(tester);
+      await tester.pumpAndSettle();
+
+      await tester.enterText(name, 'User Name');
+      await tester.enterText(accountNumber, 'LDPE123456');
+      await tester.enterText(ifscCodeField, '12340567890');
+      await tester.enterText(upiCodeField, 'someuser@apl');
+
+      await tester.tap(bankDetailsContinueState1Button);
+      await tester.pump(const Duration(milliseconds: 1000));
+      await tester.tap(bankDetailsContinueState1Button);
+
+      // Test Results
+      expect(find.byType(String), 'User Name');
+      // expect(depositButton, findsOneWidget);
+    });
+  }
